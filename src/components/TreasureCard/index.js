e1270620d7fa991cfc8cf79f80f23643372c3146
thth214 <thiagosilveirapro@gmail.com>
2021-07-13 17:33:55 -0300

    Treasure

--- src/components/TreasureCard/index.js
+++ src/components/TreasureCard/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import styled from 'styled-components'
 import {useNavigation} from '@react-navigation/native'
 import XIcon from '../../assets/x.svg'
+import TreasureContext from '../../contexts/TreasureContext'
 const Container = styled.View`
 background-color: #141414;
 flex: 1 0 21%; 
-margin: 5px;
+margin: 15px 0px;
 height: 100px;
 `;
 const FruitButton = styled.TouchableOpacity``;
@@ -17,14 +18,26 @@ border-radius: 20px;
 const DeleteButton = styled.TouchableOpacity``;
 
 const TreasureCard = ({data}) => {
-        console.log(data)
+    const {removeItem} = useContext(TreasureContext)
+    const navigation = useNavigation()
+    const handleClick = ()=> {
+        navigation.navigate('AkumaNoMi',{
+            id : data.id,
+            imagemFruta: data.imagemFruta,
+            nome: data.nome,
+            descricao: data.descricao,
+            preco: data.preco,
+            imagemUsuario: data.imagemUsuario,
+            categoria:data.categoria
+        });
+    }
     return(
         
         <Container>
-            <FruitButton>
+            <FruitButton onPress = {handleClick}>
                 <FruitImage source={{uri:data.imagemFruta}}/>
             </FruitButton>
-            <DeleteButton>
+            <DeleteButton onPress = {()=> {removeItem(data.id)}}>
                  <XIcon fill="#ffffff"/>
             </DeleteButton>
         </Container>
--- src/screens/Treasure/index.js
+++ src/screens/Treasure/index.js
@@ -1,21 +1,32 @@
 import React,{useContext} from 'react'
 import {Text} from 'react-native'
-import {Container,CardContainer} from './styles';
+import {Container,CardContainer,ButtonContainer, FinishButton,FinishText,ClearButton,ClearText, Scroller} from './styles';
 import TreasureContext from '../../contexts/TreasureContext'
 import TreasureCard from '../../components/TreasureCard';
+import {useNavigation} from '@react-navigation/native'
 
 const Home = () =>{
     const context = useContext(TreasureContext)
-
+    const {clearTreasure} = useContext(TreasureContext)
     console.log(context.treasure)
-    
+    const navigation = useNavigation();
     return(
         <Container>
+            <Scroller>
             <CardContainer>
             {context.treasure.map((fruta, k) => (
                         <TreasureCard key={k} data={fruta} />
                     ))}
             </CardContainer>
+            </Scroller>
+            <ButtonContainer>
+                <FinishButton onPress = {()=> navigation.navigate('Checkout')}>
+                    <FinishText>Finalizar Pedido</FinishText>
+                </FinishButton> 
+                <ClearButton onPress = { ()=> {clearTreasure()}}>
+                    <ClearText>Limpar Tesouro</ClearText>
+                </ClearButton>
+            </ButtonContainer>
         </Container>
     );
 }
